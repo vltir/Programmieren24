602645b39c2e68fd6902c53e89e223243392292f
vltir <87820976+vltir@users.noreply.github.com>
2024-01-15 10:21:15 +0100

    Initial Commit

---
+++ src/de/dhbwka/java/example/Main.java
@@ -0,0 +1,9 @@
+package de.dhbwka.java.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String string1 = "bla bla bla";
+        string1.split(" +");
+    }
+}
