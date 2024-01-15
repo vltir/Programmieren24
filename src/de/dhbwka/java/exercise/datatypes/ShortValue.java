095940a7b0f326e0a11fe3f6cd6f75b56065d33a
vltir <87820976+vltir@users.noreply.github.com>
2024-01-15 13:37:02 +0100

    Aufgabe zu packages und Short Value

---
+++ src/de/dhbwka/java/exercise/datatypes/ShortValue.java
@@ -0,0 +1,9 @@
+package de.dhbwka.java.exercise.datatypes;
+
+public class ShortValue {
+    public static void main(String[] args) {
+        short value = 32767;
+        value++;
+        System.out.println(value);
+    }
+}
