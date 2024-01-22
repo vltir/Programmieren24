c44f5841055ab1e36152d8ef558814eaca23ae29
vltir <87820976+vltir@users.noreply.github.com>
2024-01-22 12:15:05 +0100

    isLeapYear vereinfacht

--- src/de/dhbwka/java/exercise/control/LeapYear.java
+++ src/de/dhbwka/java/exercise/control/LeapYear.java
@@ -15,7 +15,6 @@ public class LeapYear {
     public static boolean isLeapYear(int year) {
         if (year % 400 == 0) return true;
         if (year % 100 == 0) return false;
-        if (year % 4 == 0) return true;
-        return false;
+        return year % 4 == 0;
     }
 }
