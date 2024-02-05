9b8f81b373c16db47daddeb8926fc5188bf1080d
vltir <87820976+vltir@users.noreply.github.com>
2024-02-05 08:06:21 +0100

    Babylon.java ohne funktionen hinzugefügt

---
+++ src/de/dhbwka/java/exercise/control/Babylon.java
@@ -0,0 +1,12 @@
+package de.dhbwka.java.exercise.control;
+
+public class Babylon {
+    public static void main(String[] args) {
+
+    }
+
+    public static double babylonStep(double a){
+        double next = babylonStep(a);
+        return 0.0;
+    }
+}
