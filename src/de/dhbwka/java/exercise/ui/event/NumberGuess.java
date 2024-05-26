f5e97ae233e336e549b9da80db71609d4d256c05
vltir <87820976+vltir@users.noreply.github.com>
2024-05-26 13:36:25 +0200

    created NumberGuess.

---
+++ src/de/dhbwka/java/exercise/ui/event/NumberGuess.java
@@ -0,0 +1,10 @@
+package de.dhbwka.java.exercise.ui.event;
+
+public class NumberGuess {
+    public static void main(String[] args) {
+        new NumberGuess();
+    }
+
+    public NumberGuess() {
+    }
+}
